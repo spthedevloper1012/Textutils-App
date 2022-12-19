c4004439e6d033729b46eb959876c2e8b9dbaef0
spthedevloper1012 <108466745+spthedevloper1012@users.noreply.github.com>
2022-12-19 16:30:40 +0530

    Update Navbar.js

--- src/cmp/Navbar.js
+++ src/cmp/Navbar.js
@@ -29,11 +29,7 @@ export default function Navbar(props) {
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">
-                {props.aboutText}
-              </a>
-            </li>
+            
           </ul>
           {/* <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
